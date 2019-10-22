5a5c8efb2b6afd1ce633af4cdc93eabbb358e0c2
argon-dev <argon@argon.dev>
2019-10-22 00:48:08 -0700

    Fixed windows paths in build environment.

--- docker/helium-build-env/windows/helium/launch.ps1
+++ docker/helium-build-env/windows/helium/launch.ps1
@@ -1,7 +1,10 @@
 
 $env:Path += ";$env:HELIUM_SDK_PATH"
 
-$proxy = Start-Process C:/helium/LocalhostToUnix/LocalhostToUnix.exe -ArgumentList /helium/helium.sock,9000 -PassThru
+$proxy = Start-Process C:/helium/LocalhostToUnix/LocalhostToUnix.exe -ArgumentList C:/helium/socket/helium.sock,9000 -PassThru
+
+if ( Test-Path -Path 'C:\helium\install\home\' -PathType Container ) { Copy-Item -Path 'C:\helium\install\home\*' -Destination "$env:USERPROFILE" -Recurse }
+if ( Test-Path -Path 'C:\helium\install\config\' -PathType Container ) { Copy-Item -Path 'C:\helium\install\config\*' -Destination "$env:APPDATA" -Recurse }
 
 & $args[0] $args[1..($args.Length)]
 
