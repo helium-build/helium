f558cc81d3580a59b9cfab5f11715c42ff53f94d
argon-dev <argon@argon.dev>
2019-11-18 10:10:51 -0500

    Updated run script.

--- examples/print-version/run.sh
+++ examples/print-version/run.sh
@@ -3,7 +3,9 @@ cd "$(readlink -f "$(dirname "$0")")"/../../
 mkdir -p examples/print-version/src/
 mkdir -p examples/print-version/output/
 
+export HELIUM_CONTENT_ROOT="$(pwd)/src/Engine/"
+
 for sdk in examples/print-version/*.toml; do
   echo "SDK: $sdk"
-  java -jar target/scala-2.13/Helium-assembly-0.1.0-SNAPSHOT.jar build-once "$sdk" examples/print-version/src/ examples/print-version/output/
+  dotnet run -p src/Engine build --schema "$sdk" examples/print-version/
 done
