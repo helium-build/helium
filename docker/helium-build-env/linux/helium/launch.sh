17bbe4ee399a32720e85813c5242d3e5ae70f21a
argon-dev <argon@argon.dev>
2019-11-22 07:06:38 -0500

    Fixed ignored error codes.

--- docker/helium-build-env/linux/helium/launch.sh
+++ docker/helium-build-env/linux/helium/launch.sh
@@ -9,5 +9,8 @@ if [ -d "/helium/install/home" ]; then cp -rT /helium/install/home $HOME; fi
 if [ -d "/helium/install/config" ]; then cp -rT /helium/install/config $HOME/.config; fi
 
 "$@"
+EXIT_CODE=$?
 
 kill $SOCAT_PID
+
+exit $EXIT_CODE
